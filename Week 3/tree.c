76cc008dd69c51e130292d2b35a258db510e5858
SouuYe07 <iesouaguila07@gmail.com>
2026-07-02 22:14:15 +0800

    Week 3

---
+++ Week 3/tree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+typedef struct tree_node{
+    int data;
+    struct tree_node* first_child;
+    struct tree_node* second_child;
+} tree_ptr;
+
+int main(){
+    return 0;
+}
