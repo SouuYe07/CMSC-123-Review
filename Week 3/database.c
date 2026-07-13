97335dd7a11b8180b5ec08e2b8b2835c3ae989f3
SouuYe07 <iesouaguila07@gmail.com>
2026-07-13 13:34:55 +0800

    Redo database.c

--- Week 3/database.c
+++ Week 3/database.c
@@ -1,41 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct node{
-    int id;
-    int height;
-    struct node *leftChild;
-    struct node *rightChild; 
-};
-
-
-
-void insert(struct node* head, int id){
-    if (head == NULL){
-        head = (struct node*)malloc(sizeof(struct node));
-        head->leftChild = NULL;
-        head->rightChild = NULL;
-        head->id = id;
-        head->height = -1;
-    }
-    else if(id < head->id) insert(head->leftChild, id);
-    else if(id > head->id) insert(head->rightChild, id);
-    head->height++;
-}
-
-void insertUser(struct node** head){
-    int id, quantity;
-    printf("Enter User ID: ");
-    scanf("%d", &id);
-
-    insert(*head, id);
-}
 
 int main(void){
     int choice;
-    struct node* head = NULL;
 
-    while(1){
+    while (1){
         printf("*********************************\n");
         printf("Welcome to the MazeDB Indexing Engine!\n");
         printf("*********************************\n");
@@ -45,10 +13,5 @@ int main(void){
         printf("Enter choice: ");
         scanf("%d", &choice);
 
-        switch (choice){
-            case 1:
-                insertUser(&head);
-        }
     }
-    return 0;
 }
