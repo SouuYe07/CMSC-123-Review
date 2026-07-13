5975d1d3f6349aa0f6d065f885e6158194783f70
SouuYe07 <iesouaguila07@gmail.com>
2026-07-13 13:41:24 +0800

    Added Node struct and half of insert

--- Week 3/database.c
+++ Week 3/database.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int id;
+    int height;
+    struct Node *leftChild;
+    struct Node *rightChild;
+};
+
+struct Node* initNode(){
+    struct Node *init = (struct Node *)malloc(sizeof(struct Node));
+    init->leftChild = NULL;
+    init->rightChild = NULL;
+}
+
+struct Node *insertNode(struct Node *head){
+    int id;
+    printf("Enter Used ID: ");
+    scanf("%d", &id);
+
+    printf("User %i indexed.\n", id);
+}
 
 int main(void){
     int choice;
+    struct Node *head = NULL;
 
     while (1){
         printf("*********************************\n");
@@ -13,5 +36,10 @@ int main(void){
         printf("Enter choice: ");
         scanf("%d", &choice);
 
+        switch(choice){
+            case 1:
+                head = insertNode(head);
+        }
+        printf("\n");
     }
 }
