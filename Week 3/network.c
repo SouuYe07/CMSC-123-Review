4424ca9f3acb91ae8c30a8cc12f8a7ddb5454162
SouuYe07 <iesouaguila07@gmail.com>
2026-07-16 22:35:04 +0800

    Menu

--- Week 3/network.c
+++ Week 3/network.c
@@ -7,6 +7,33 @@ struct Node{
     struct Node* rightChild;
 };
 
+struct Node* initNode(){
+    struct Node* init = (struct Node*) malloc(sizeof(struct Node));
+    init->leftChild = NULL;
+    init->rightChild = NULL;
+}
+
 int main(void){
+    int choice;
+    struct Node* head = initNode();
+
+    while (1) {
+        printf("*********************************\n");
+        printf("Welcome to the MazeRoute Cache Engine!\n");
+        printf("*********************************\n");
+        printf("[1] Access / Insert Routing IP\n");
+        printf("[2] Search Cache\n");
+        printf("[3] Show Curernt Cache Root\n");
+        printf("[4] Display Cache Hierarchy (Inorder)\n");
+        printf("[5] Exit\n\n");
+
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice){
+            case 1:
+                break;
+        }
+    }
     return 0;
 }
